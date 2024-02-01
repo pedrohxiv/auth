e444956b9c36b1d8bfe7ff15310bc84be7d1e13f
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-02-01 17:49:59 -0300

    Create user created

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,36 @@
 "use server";
 
+import bcrypt from "bcryptjs";
 import * as z from "zod";
 
-import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
 
-export const register = async (values: z.infer<typeof LoginSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "User successfully registered!" };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use! " };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  return { success: "User created successfully!" };
 };
